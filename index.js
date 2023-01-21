c7e04c05c18578fba963d4dbecb375f9dfe4d7ce
Kyra Nankivell <kyranankivell@Kyras-MacBook-Pro.local>
2023-01-20 19:19:35 -0500

    feat: first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ msg: "get guests from monogodb" });
+});
+
+app.post("/", (req, res) => {
+  res.send({ msg: "set rsvp to true" });
+});
+
+app.get("/add-guest", (req, res) => {
+  res.send({ msg: "serve the add guest form" });
+});
+
+app.post("/add-guest", (req, res) => {
+  res.send({ msg: "add guest to database" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
