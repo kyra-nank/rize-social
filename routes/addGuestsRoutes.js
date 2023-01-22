3a613c396bebeb448211fbcf945d60731d7d1e32
Kyra Nankivell <kyranankivell@Kyras-MacBook-Pro.local>
2023-01-21 22:21:24 -0500

    feat: complete mongo routes

--- routes/addGuestsRoutes.js
+++ routes/addGuestsRoutes.js
@@ -6,27 +6,31 @@ module.exports = (app) => {
 
   app.get("/add-guest", (req, res) => {
     res.send({ msg: "serve add guest form" });
-
-    // avoid duplicate records
-    Guest.findOne({ pin: "13282" })
-      .then((existingGuest) => {
-        if (!existingGuest) {
-          // guest does not exist in db
-          new Guest({
-            pin: "13282",
-            rsvp: false,
-            name: "Kyra Nankivell",
-            image: "https://media.istockphoto.com/id/1208585275/photo/profile-of-a-serene-young-woman.jpg?s=612x612&w=0&k=20&c=GU_NrBvXEiKL8GDe6MIVmx_M3xzwMKzjJ27RI0rHmw8=",
-            linkedIn: "https://www.linkedin.com/in/kyranank/",
-            instagram: "https://www.instagram.com/kyra_nank/"
-          }).save()
-        }
-      })
-
   });
 
   app.post("/add-guest", (req, res) => {
-    res.send({ msg: "add guest to database" });
+    // add guest to db from form inputs
+    const inputPin = "13282";
+    const name = "Kyra Nankivell";
+    const image = "https://media.istockphoto.com/id/1208585275/photo/profile-of-a-serene-young-woman.jpg?s=612x612&w=0&k=20&c=GU_NrBvXEiKL8GDe6MIVmx_M3xzwMKzjJ27RI0rHmw8=";
+    const linkedIn = "https://www.linkedin.com/in/kyranank/";
+    const instagram = "https://www.instagram.com/kyra_nank/";
+
+    // avoid duplicate records
+    Guest.findOne({ pin: inputPin })
+      .then((foundGuest) => {
+        if (!foundGuest) {
+          // guest does not exist in db
+          new Guest({
+            pin: inputPin,
+            rsvp: false,
+            name: name,
+            image: image,
+            linkedIn: linkedIn,
+            instagram: instagram
+          }).save()
+        }
+      })
   });
 
 };
--- routes/guestListRoutes.js
+++ routes/guestListRoutes.js
@@ -5,24 +5,30 @@ const Guest = mongoose.model('guests');
 module.exports = (app) => {
 
   app.get("/", (req, res) => {
-    res.send({ msg: "get guests from monogodb" });
-
-
+    // get all the records from db & send to frontend
+    Guest.find({})
+      .then((guests) => {
+        res.send(guests);
+      });
 
   });
 
   app.post("/", (req, res) => {
-    res.send({ msg: "set rsvp to true" });
+    // set rsvp to true if the pin is correct - assuming no one misenters a pin that corr. to another user
+    const inputPin = "13282"
 
     // find user with the entered pin & change rsvp to true
-    Guest.updateOne({ pin: "13282" }, { rsvp: true })
+    Guest.updateOne({ pin: inputPin }, { rsvp: true })
       .then((doc) => {
-        if (doc.modifiedCount == 0) {
+        if (doc.modifiedCount == 1) {
+          // user rsvped
+          res.send({ msg: "success - you have been rsvped!" })
+        } else {
           // no docs modified - pin not found
           // since there is no way for someone to try rsvping again, no need to handle the case where the record is already set to true
-          console.log("incorrect pin")
+          res.send({ msg: "incorrect - please try again" })
         }
-      })
+      });
 
   });
 
